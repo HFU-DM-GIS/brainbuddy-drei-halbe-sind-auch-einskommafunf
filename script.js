be0ee75443c790e3c5d759f6f53d00ba3211ed66
xbenedikt <mohr.benno@gmail.com>
2023-12-07 03:17:20 +0100

    api-integration & selector

--- script.js
+++ script.js
@@ -1,72 +1,128 @@
-const cards = document.querySelectorAll(".card");
+
+
+let cards = document.querySelectorAll(".card");
 const resetButton = document.querySelector("#reset");
 let flippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  if (!flippedCard) {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    console.log(this.classList)
     this.classList.add("selected");
-    console.log(this.firstChild);
-    this.firstChild.classList.add("flipped");
-    flippedCard = true;
-    firstCard = this;
-    return;
-  }
-  this.classList.add("selected");
-  this.firstChild.classList.add("flipped");
-  secondCard = this;
-  checkForMatch();
+    this.classList.add("flipped");
+    console.log(this.classList)
+    if (!flippedCard) {
+        flippedCard = true;
+        firstCard = this;
+    } else {
+        secondCard = this;
+        checkForMatch();
+    }
 }
 
 function checkForMatch() {
-  if (firstCard.dataset.card === secondCard.dataset.card) {
-    disableCards();
-    return;
-  }
+    const isMatch = firstCard.dataset.card === secondCard.dataset.card;
 
-  unflipCards();
+    isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
 
-  firstCard.classList.add("correct");
-  secondCard.classList.add("correct");
-  resetBoard();
+    firstCard.classList.add("correct");
+    secondCard.classList.add("correct");
+    resetBoard();
 }
 
 function unflipCards() {
-  lockBoard = true;
-  firstCard.classList.add("incorrect");
-  secondCard.classList.add("incorrect");
-
-  setTimeout(() => {
-    firstCard.classList.remove("selected", "incorrect");
-    secondCard.classList.remove("selected", "incorrect");
-    firstCard.firstChild.classList.remove("flipped");
-    secondCard.firstChild.classList.remove("flipped");
-    resetBoard();
-  }, 1000);
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove("selected");
+        secondCard.classList.remove("selected");
+        firstCard.firstChild.classList.remove("flipped");
+        secondCard.firstChild.classList.remove("flipped");
+        resetBoard();
+    }, 1000);
 }
 
 function resetBoard() {
-  [flippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+    [flippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
-resetButton.addEventListener("click", () => {
-  location.reload();
-});
+function resetGame() {
+    cards.forEach(card => {
+        card.classList.remove("selected", "correct");
+        card.firstChild.classList.remove("flipped");
+        card.addEventListener('click', flipCard);
+    });
 
-(function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
-})();
+    [flippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+
+function getTheme() {
+    return document.getElementById('theme-input').value.trim();
+}
+
+//
+const numberSelect = document.getElementById('numberSelect');
+const resultElement = document.getElementById('result');
+
+function updateValue() {
+  const selectedValue = parseInt(numberSelect.value);
+  console.log('Eingegebener Wert:', selectedValue);
+  return selectedValue;
+}
+//
+
+async function getImages(apiUrl) {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Bilder:', error);
+    }
+}
+function createCard(imageUrl, index) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.card = index;
+
+    const frontSide = document.createElement('div');
+    frontSide.classList.add('card-side', 'front-side');
+    frontSide.style.backgroundImage = 'url("/images/fun-memory.png")';
+
+    const backSide = document.createElement('div');
+    backSide.classList.add('card-side', 'back-side');
+    backSide.style.backgroundImage = `url(${imageUrl})`;
+    card.appendChild(frontSide);
+    card.appendChild(backSide);
+
+    card.addEventListener('click', flipCard);
+
+    return card;
+}
+
+async function initializeGame() {
+    resetGame(); // Füge diese Zeile hinzu, um das Spiel zurückzusetzen, bevor du neue Karten erstellst
+    const value = updateValue();
+    const theme = getTheme();
+    const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apikey}&count=${value}&query=${theme}`;
+
+    const images = await getImages(apiUrl);
+    const doubledImages = [...images, ...images];
+    doubledImages.sort(() => Math.random() - 0.5);
+
+    const memoryGame = document.querySelector('.memory-game');
+    cards = document.querySelectorAll(".card"); // Aktualisiere die Karten nach dem Reset
+    doubledImages.forEach((image, index) => {
+        const card = createCard(image.urls.small, index);
+        memoryGame.appendChild(card);
+    });
+}
